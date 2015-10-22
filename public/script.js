7d49f6f636cae99cb610b325d108ca2e7a853815
Emily Landi <emily.landi@gmail.com>
2015-10-22 16:10:02 -0700

    changes to website, added js feature to countdown to winter break

---
+++ public/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  var day = new Date("December 21, 2015");
+  var now = new Date();
+  var timeDiff = day.getTime() - now.getTime();
+  if (timeDiff <= 0) {
+    clearTimeout(timer);
+    document.write("Enjoy Winter Break!");
+  }
+
+  var seconds = Math.floor(timeDiff / 1000);
+  var minutes = Math.floor(seconds / 60);
+  var hours = Math.floor(minutes / 60);
+  var days = Math.floor(hours / 24);
+
+  $("#countdown").ready(function(){
+    $("#countdown").append(days + " days.");
+
+  var timer = setTimeout('function()',1000);
+
+  });
+});
